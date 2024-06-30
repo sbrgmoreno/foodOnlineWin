88a6be70559e8dbfc84983c626cb2cecc9073079
sbrgmoreno <pimgmp@gmail.com>
2024-06-30 09:24:13 -0500

    register user functionality and home page setup

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
+    'django.contrib.messages',  #messages installed
     'django.contrib.staticfiles',
     
     'accounts',
@@ -143,3 +143,8 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
