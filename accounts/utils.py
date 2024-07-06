1c17d709620f9f47bbb6e2d7ab67a4d3b413e41a
sbrgmoreno <pimgmp@gmail.com>
2024-07-06 16:31:30 -0500

    toke verification

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,4 +1,9 @@
-
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 
 def detectUser(user):
@@ -11,4 +16,17 @@ def detectUser(user):
     elif user.role == None and user.is_superadmin:
         redirectUrl = '/admin'
         return redirectUrl
-    
+
+def send_verification_email(request, user):
+    #for now the site is localhost 127.0.0.1
+    current_site = get_current_site(request)
+    mail_subject = 'Please active your account'
+    message = render_to_string('accounts/emails/account_verification_email.html', {
+        'user': user,
+        'domain': current_site,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)), #encode in the email template
+        'token': default_token_generator.make_token(user),
+    })
+    to_email = user.email
+    mail = EmailMessage(mail_subject, message, to=[to_email])
+    mail.send()
--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from vendor.forms import VendorForm
 from .forms import UserForm
 from .models import User, UserProfile
 from django.contrib import messages, auth
-from .utils import detectUser
+from .utils import detectUser, send_verification_email
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 # Create your views here.
@@ -48,6 +48,10 @@ def registerUser(request):
             user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
+            
+            #send verification email
+            send_verification_email(request, user)
+            
             messages.success(request, 'Your account has been registered successfuly')
             return redirect('registerUser')
         else:
@@ -82,6 +86,10 @@ def registerVendor(request):
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
+            
+            #send verification email
+            send_verification_email(request, user)
+            
             messages.success(request, 'Your account has been registered successfuly! Please wait for the approval.')
             return redirect('registerVendor')
         else:
@@ -96,6 +104,10 @@ def registerVendor(request):
         'v_form': v_form,
     }
     return render(request, 'accounts/registerVendor.html', context)
+
+def activate(request, uidb64, token):
+    #activate the user by setting the is_activate status to True
+    return 
 
 def login(request):
     if request.user.is_authenticated:
