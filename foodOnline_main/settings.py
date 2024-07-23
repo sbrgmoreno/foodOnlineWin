e0805a49f0360d01e0ae25e4879efd02df3971bf
sbrgmoreno <pimgmp@gmail.com>
2024-07-22 21:14:05 -0500

    smtp configuration

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -155,16 +155,10 @@ MESSAGE_TAGS = {
 
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
-EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'store.online.ecuador@gmail.com'
+EMAIL_HOST_PASSWORD = 'pdft yxux snuc dtuv'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <store.online.ecuador@gmail.com>'
 
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_PORT = 587
-#EMAIL_HOST_USER = 'store.online.ecuador@gmail.com'
-#EMAIL_HOST_PASSWORD = 'eiui aoiw wkek yzdn'
-#EMAIL_USE_TLS = True
-#DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <store.online.ecuador@gmail.com>'
